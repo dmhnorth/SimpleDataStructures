2864c7af630bde93bc9f4a9542c54c0b93f68564
David <dmhnorth@gmail.com>
2014-11-03 13:35:46 +0000

    isEmpty, size, and iterator are all implemented.

--- BagStackQueue/src/interfaces/StackImpl.java
+++ BagStackQueue/src/interfaces/StackImpl.java
@@ -3,8 +3,15 @@ package interfaces;
 import java.util.Iterator;
 
 public class StackImpl<Item> implements Stack<Item> {
+    private Node head = null;
+
     @Override
-    public void push(Object o) {
+    public void push(Item item) {
+        Node node = new Node(item);
+
+        if(isEmpty()){
+            head = node;
+        }
 
     }
 
@@ -15,16 +22,51 @@ public class StackImpl<Item> implements Stack<Item> {
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return head == null;
     }
 
     @Override
     public int size() {
-        return 0;
+        int count = 0;
+
+        Node current = head;
+
+        if(head != null){
+            count++;
+            while(current.linkedNode != null){
+                count++;
+                current = current.linkedNode;
+            }
+        }
+        return count;
     }
 
     @Override
-    public Iterator iterator() {
-        return null;
+    public Iterator<Item> iterator() {
+        return new Iterator<Item>() {
+
+            Node currentNode = head;
+
+            @Override
+            public boolean hasNext() {
+                return currentNode != null;
+            }
+
+            @Override
+            public Item next() {
+                Item item = currentNode.item;
+                currentNode = currentNode.linkedNode;
+                return item;
+            }
+        };
+    }
+
+    class Node{
+        private Item item;
+        private Node linkedNode;
+
+        Node(Item item) {
+            this.item = item;
+        }
     }
 }
